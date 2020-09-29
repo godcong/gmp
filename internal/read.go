3b411735d643ec253808adb1079c30d6ef81ebda
godcong <jumbycc@163.com>
2020-09-29 14:46:32 +0800

    add

--- internal/read.go
+++ internal/read.go
@@ -4,30 +4,62 @@ import (
 	"bufio"
 	"github.com/pkg/errors"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 )
+
+type BackupCallback func([]byte) []byte
 
 var modName = "go.mod"
 var backupName = modName + ".old"
 var ErrFileIsAlreadyExist = errors.New("target file is already exist")
 
-func UpdateMod(target string) (string, error) {
-	path := GetCurrentPath()
-	back := filepath.Join(path, backupName)
-	open, err := os.Open(back)
+func UpdateGoFile(path string, src, target string) error {
+	current := GetCurrentPath()
+	dir, err := ioutil.ReadDir(current)
+	if err != nil {
+		return err
+	}
+	for _, info := range dir {
+		if info.IsDir() {
+			if err := UpdateGoFile(filepath.Join(path, info.Name()), src, target); err != nil {
+				return err
+			}
+			continue
+		}
+		if filepath.Ext(info.Name()) == ".go" {
+			if err := backupFile(path, info.Name(), info.Name()+".old"); err != nil {
+				return err
+			}
+
+			if err := fixImport(filepath.Join(path, info.Name()), src, target); err != nil {
+				return err
+			}
+			continue
+		}
+		//skip
+	}
+	return nil
+}
+
+func fixImport(path string, srcModule string, targetModule string) error {
+	//todo
+	return nil
+}
+
+func FileBackup(src, target string, cb BackupCallback) (string, error) {
+	open, err := os.Open(src)
 	if err != nil {
 		return "", err
 	}
 	defer open.Close()
 	reader := bufio.NewReader(open)
-	file, err := os.OpenFile(filepath.Join(path, modName), os.O_TRUNC|os.O_CREATE|os.O_RDWR|os.O_SYNC, 0755)
+	file, err := os.OpenFile(target, os.O_TRUNC|os.O_CREATE|os.O_RDWR|os.O_SYNC, 0755)
 	if err != nil {
 		return "", err
 	}
 	defer file.Close()
-	var src string
 	for {
 		line, _, err := reader.ReadLine()
 		if err != nil {
@@ -36,11 +68,8 @@ func UpdateMod(target string) (string, error) {
 			}
 			return "", err
 		}
-		if strings.Index(string(line), "module") >= 0 {
-			if s := strings.Split(string(line), " "); len(s) > 1 {
-				src = s[len(s)-1]
-			}
-			line = fixModule(target)
+		if cb != nil {
+			line = cb(line)
 		}
 		if _, err := file.Write(line); err != nil {
 			return "", err
@@ -50,11 +79,17 @@ func UpdateMod(target string) (string, error) {
 }
 
 func fixModule(target string) []byte {
+	//if strings.Index(string(line), "module") >= 0 {
+	//	if s := strings.Split(string(line), " "); len(s) > 1 {
+	//		src = s[len(s)-1]
+	//	}
+	//	line = fixModule(target)
+	//}
 	return []byte("module " + target)
 }
 
-func backupMod(path string) error {
-	back := filepath.Join(path, backupName)
+func backupFile(path string, src, target string) error {
+	back := filepath.Join(path, target)
 	_, err := os.Stat(back)
 	if err != nil {
 		if !os.IsNotExist(err) {
@@ -65,7 +100,7 @@ func backupMod(path string) error {
 		return ErrFileIsAlreadyExist
 	}
 
-	if err := os.Rename(filepath.Join(path, modName), back); err != nil {
+	if err := os.Rename(filepath.Join(path, src), back); err != nil {
 		return err
 	}
 	return nil
